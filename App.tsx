7d0a58714316c64bbf9a1ade9e5cfa6daf7c074e
6joeb <joe6ball@gmail.com>
2023-10-25 18:36:35 +0100

    Refactored via moving screens and helpers into own files

--- App.tsx
+++ App.tsx
@@ -1,47 +1,9 @@
-import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-
-const storeData = async (key, value) => {
-  try {
-    await AsyncStorage.setItem(key, value);
-  } catch (e) {
-    // saving error
-  }
-};
-
-const getData = async key => {
-  try {
-    const value = await AsyncStorage.getItem(key);
-    if (value !== null) {
-      // value previously stored
-    }
-  } catch (e) {
-    // error reading value
-  }
-};
+import AddHabitScreen from './Screens/AddHabitScreen';
+import HomeScreen from './Screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -50,7 +12,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Add Habit" component={AddHabitScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Screens/AddHabitScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Button, TextInput} from 'react-native';
+
+const AddHabitScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Add Habit</Text>
+    </View>
+  );
+};
+
+export default AddHabitScreen;
