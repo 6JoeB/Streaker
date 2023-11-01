dadabee70eef0bc46fd1105e1310d9f75ef127e0
6joeb <joe6ball@gmail.com>
2023-11-01 18:34:19 +0000

    Fixed duplicating habits error and amended error messages

--- Helpers/AsyncStorage.tsx
+++ Helpers/AsyncStorage.tsx
@@ -5,7 +5,7 @@ export const storeDataObject = async (key: string, value: object) => {
     const jsonValue = JSON.stringify(value);
     await AsyncStorage.setItem(key, jsonValue);
   } catch (e) {
-    console.error('Error storing data object in AsyncStorage, error: ' + e);
+    console.error('Error storing data object in AsyncStorage, ' + e);
   }
 };
 
@@ -14,30 +14,21 @@ export const getDataObject = async (key: string, data: any[], setData: any) => {
     const jsonValue = await AsyncStorage.getItem(key);
     return jsonValue != null ? setData([...data, JSON.parse(jsonValue)]) : null;
   } catch (e) {
-    console.error('Error returning data object in AsyncStorage, error: ' + e);
+    console.error('Error returning data object in AsyncStorage, ' + e);
   }
 };
 
 export const getDataObjects = async (keys, data, setData) => {
   try {
     const values = await Promise.all(
-      keys.map(key => AsyncStorage.getItem(key).then(data => JSON.parse(data))),
+      keys.map(key =>
+        AsyncStorage.getItem(key).then(jsonData => JSON.parse(jsonData)),
+      ),
     );
 
-    if (data !== undefined) {
-      let i = values.length;
-      while (i--) {
-        data.forEach(element => {
-          if (values[i].name === element.name) {
-            values.splice(i, 1);
-          }
-        });
-      }
-    }
-
-    setData(prev => [...prev, ...values.filter(item => item !== null)]);
+    setData(values.filter(item => item !== null));
   } catch (e) {
-    console.error('Error returning data objects in AsyncStorage, error: ' + e);
+    console.error('Error returning data objects in AsyncStorage, ' + e);
   }
 };
 
@@ -46,9 +37,7 @@ export const getAllKeys = async (setAsyncStorageKeys: any) => {
   try {
     keys = await AsyncStorage.getAllKeys();
   } catch (e) {
-    console.error(
-      'Error returning all data objects in AsyncStorage, error: ' + e,
-    );
+    console.error('Error returning all keys in AsyncStorage, ' + e);
   }
   setAsyncStorageKeys(keys);
 };
--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
 
 import {getAllKeys, getDataObjects} from '../Helpers/AsyncStorage';
 
@@ -7,27 +8,29 @@ const HomeScreen = ({navigation, route}) => {
   const [habits, setHabits] = useState([]);
   const [asyncStorageKeys, setAsyncStorageKeys] = useState([]);
 
-  useEffect(() => {
-    getAllKeys(setAsyncStorageKeys);
-  }, []);
+  const isFocused = useIsFocused();
 
   useEffect(() => {
-    getDataObjects(asyncStorageKeys, setHabits);
+    if (isFocused) {
+      getAllKeys(setAsyncStorageKeys);
+    }
+  }, [isFocused]);
+
+  useEffect(() => {
+    getDataObjects(asyncStorageKeys, habits, setHabits);
   }, [asyncStorageKeys]);
-
-  useEffect(() => {
-    console.log(habits);
-  }, [habits]);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      {/* {asyncStorageKeys.length > 0 && habits[1].length > 0 ? (
-        habits[1].forEach(item => {
-          <Text>hello</Text>;
-        })
+      {asyncStorageKeys.length > 0 && habits.length > 0 ? (
+        habits.map(habit => (
+          <Text>
+            {habit.name} {habit.daysPerWeek}
+          </Text>
+        ))
       ) : (
         <Text>Habits will show here once one is created</Text>
-      )} */}
+      )}
       <Button
         title="Add Habit"
         onPress={() => navigation.navigate('Add Habit')}
