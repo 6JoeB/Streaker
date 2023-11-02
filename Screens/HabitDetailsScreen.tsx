2cb648188cb54436a1c55178b0005af1e583c5c5
6joeb <joe6ball@gmail.com>
2023-11-02 14:31:51 +0000

    Habit calendar feature WIP

--- Screens/HabitDetailsScreen.tsx
+++ Screens/HabitDetailsScreen.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
 import {removeValue} from '../Helpers/AsyncStorage';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
 
 export const HabitDetailsScreen = ({navigation, route}) => {
   const {name, daysPerWeek} = route.params;
+
+  const [selected, setSelected] = useState('');
+
+  useEffect(() => {
+    console.log(selected);
+  }, [selected]);
 
   const deleteHabit = async () => {
     const success = await removeValue(name);
@@ -16,6 +23,18 @@ export const HabitDetailsScreen = ({navigation, route}) => {
     <View>
       <Text>{name}</Text>
       <Text>{daysPerWeek}</Text>
+      <Calendar
+        onDayPress={day => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedColor: 'orange',
+          },
+        }}
+      />
       <Button title="Delete Habit" onPress={() => deleteHabit()} />
     </View>
   );
