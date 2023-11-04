f27c1789c8504cf9decd3a38b9916a1139b934ed
6joeb <joe6ball@gmail.com>
2023-11-04 15:30:40 +0000

    Now accurately calculates current streak regardless of previous streaks

--- Screens/HabitDetailsScreen.tsx
+++ Screens/HabitDetailsScreen.tsx
@@ -91,16 +91,25 @@ export const HabitDetailsScreen = ({navigation, route}) => {
     // Check if there is a streak within the allowed missing days
     const allowedMissingDays: number = 7 - habit.daysPerWeek;
     let streak: number = 0;
+    let dateToCheckStreakFromString: string = ascendingCompletedDays[0];
 
     while (ascendingCompletedDays.length > 0) {
       let weeksDates: string[] = [];
 
       for (let day = 0; day < 7; day++) {
-        let weekDate = new Date(ascendingCompletedDays[0]);
+        let weekDate = new Date(dateToCheckStreakFromString);
         weekDate.setDate(weekDate.getDate() + day);
         const stringDate = weekDate.toISOString().slice(0, 10);
         weeksDates.push(stringDate);
       }
+
+      let dateToCheckStreakFromDate = new Date(dateToCheckStreakFromString);
+      dateToCheckStreakFromDate.setDate(
+        dateToCheckStreakFromDate.getDate() + 7,
+      );
+      dateToCheckStreakFromString = dateToCheckStreakFromDate
+        .toISOString()
+        .slice(0, 10);
 
       let weeksMissedDates: number = 0;
 
@@ -111,7 +120,10 @@ export const HabitDetailsScreen = ({navigation, route}) => {
         }
 
         // Check if the date has been missed in a week time span
-        if (!ascendingCompletedDays.includes(date)) {
+        if (
+          !ascendingCompletedDays.includes(date) &&
+          date !== new Date().toISOString().slice(0, 10)
+        ) {
           weeksMissedDates++;
         }
 
@@ -124,8 +136,11 @@ export const HabitDetailsScreen = ({navigation, route}) => {
       });
 
       // Set streak to 0 if too many days have been missed
-      if (weeksMissedDates > allowedMissingDays + 1) {
+      if (weeksMissedDates > allowedMissingDays) {
+        console.log(weeksMissedDates);
+        console.log(allowedMissingDays);
         streak = 0;
+        dateToCheckStreakFromString = ascendingCompletedDays[0];
       }
     }
 
