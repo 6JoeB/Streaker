efbc945de046c786257d57b1179c6ee2f91885be
6joeb <joe6ball@gmail.com>
2023-11-09 23:14:34 +0000

    Refactoring

--- widgets/StreakWidget.tsx
+++ widgets/StreakWidget.tsx
@@ -1,94 +1,38 @@
 import React from 'react';
 import {FlexWidget, TextWidget} from 'react-native-android-widget';
+import {
+  streakWarningLevels,
+  updateStreakWarningLevel,
+} from '../utils/StreakWidgetHelper';
 
 // todo:
-// make widget smaller
+// styling
 // onclick widget load app
 // choose which habit to show
-// messages as day gets later to complete task...
-// ...check habit data not cleared after update,times: 10pm RED 8pm orange 6pm yellow
+// pass habit on create widget
+// check habit data not cleared after update
 // update image for widget
 
-// type HabitProps = {
-//   name: string;
-//   daysPerWeek: string[];
-//   completedDays: [];
-//   currentStreak: number;
-//   bestStreak: number;
-//   totalDaysCompleted: number;
-// };
+type HabitProps = {
+  name: string;
+  daysPerWeek: number;
+  completedDays: [];
+  currentStreak: number;
+  bestStreak: number;
+  totalDaysCompleted: number;
+};
 
-export function StreakWidget({habit}) {
+export function StreakWidget({habit}: HabitProps) {
   let habitLoaded: boolean = false;
 
-  const streakWarningLevels: object[] = [
-    {
-      color: 'green',
-      message: "Don't forget to make time today for good habits",
-    },
-    {
-      color: 'yellow',
-      message: 'Only 6 hours left!',
-    },
-    {
-      color: 'orange',
-      message: 'Only 4 hours left!',
-    },
-    {
-      color: 'red',
-      message: 'Only 2 hours left!',
-    },
-  ];
+  let streakWarningLevel: number;
 
-  let streakWarningLevel: number = 0;
-
-  if (Object.hasOwn(habit, 'name')) {
+  if (habit !== undefined && Object.hasOwn(habit, 'name')) {
+    console.log(habit);
+    streakWarningLevel = updateStreakWarningLevel(habit);
     habitLoaded = true;
   }
 
-  const updateStreakWarningLevels = () => {
-    const today = new Date();
-    const timeNow = new Date().getTime();
-
-    const sixPM = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      18,
-    ).getTime();
-
-    if (
-      habit.completedDays.includes(today.toISOString().slice(0, 10)) ||
-      timeNow < sixPM
-    ) {
-      streakWarningLevel = 0;
-      return;
-    }
-
-    const eightPM = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      20,
-    ).getTime();
-
-    const tenPM = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      22,
-    ).getTime();
-
-    if (timeNow >= tenPM) {
-      streakWarningLevel = 3;
-    } else if (timeNow >= eightPM) {
-      streakWarningLevel = 2;
-    } else if (timeNow >= sixPM) {
-      streakWarningLevel = 1;
-    }
-  };
-
-  updateStreakWarningLevels();
   return (
     <FlexWidget
       style={{
@@ -96,8 +40,8 @@ export function StreakWidget({habit}) {
         width: 'match_parent',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#ffffff',
         borderRadius: 16,
+        backgroundColor: streakWarningLevels[streakWarningLevel].color,
       }}>
       {habitLoaded ? (
         <FlexWidget>
